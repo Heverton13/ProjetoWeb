63cd498485522689d53e0f102bdb51592e736622
Heverton13 <heverton.gomes2018@gmail.com>
2019-06-25 01:41:29 -0300

    Filtro Funcionando

---
+++ src/main/java/com/ufrn/filtro/FiltroAdmin.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ufrn.filtro;
+
+import com.ufrn.model.Login;
+import com.ufrn.model.User;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Dev
+ */
+@WebFilter("/admin/*")
+public class FiltroAdmin implements Filter {
+    
+      /**
+     * Default constructor. 
+     */
+    public FiltroAdmin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		Login u = null;
+		
+		HttpSession session = ((HttpServletRequest) request).getSession(false);
+		
+		if(session != null){
+			u = (Login) session.getAttribute("admin-logado");
+		}
+		
+		if (u == null){
+			String contextPath = ((HttpServletRequest) request).getContextPath();
+			((HttpServletResponse) response).sendRedirect(contextPath + "/index.xhtml");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+    
+}
